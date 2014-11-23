4d55544672396fde4856095bd5396b8414cebc4b
bp-inter.net <kontakt@bp-inter.net>
2014-11-23 19:34:57 +0100

    wake up device

--- app/src/main/java/eu/pi1/bpwecker/bpwecker/AlarmActivity.java
+++ app/src/main/java/eu/pi1/bpwecker/bpwecker/AlarmActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import java.util.GregorianCalendar;
@@ -19,6 +20,11 @@ public class AlarmActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onalarm);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 
 
